f70265c05a28f9bab0b3859e9106f32c0e3edd8c
jaybharadia <jay.p.bharadia@gmail.com>
2023-05-17 12:55:03 +0545

    WIP

--- src/plugin.js
+++ src/plugin.js
@@ -1,16 +1,41 @@
+import { isVue2, isVue3, reactive } from "vue-demi";
 import HelloWorld from "@/components/HelloWorld.vue";
 export default {
   install: (app, options) => {
     // Plugin code goes here
 
-    app.component('DHello', HelloWorld);
+    console.log("🚀 ~ file: plugin.js:19 ~ reactive:", reactive)
 
-    app.config.globalProperties.$demi = {
-        name : 'vue-demi-demo',
-        start : () => {
-            console.log('Inside Demo Global Property method');
+    
+
+    // app.component('DHello', HelloWorld);
+
+    if( isVue2){
+        console.log('insdie isVUE 2');
+    const VueInstance = new app();
+    console.log("🚀 ~ file: plugin.js:15 ~ VueInstance:", VueInstance)
+    
+        VueInstance.prototype.$demi = {
+            name : 'vue-demi-demo',
+            start : () => {
+                console.log('Start method init ');
+            }
+        };
+    }else if( isVue3 ){
+        console.log('insdie isVUE 3');
+
+
+        app.config.globalProperties.$demi = {
+            name : 'vue-demi-demo',
+            start : () => {
+                console.log('Start method init ');
+            }
         }
-    }
+      
+
+
+
+    }else{}
 
   }
 }
